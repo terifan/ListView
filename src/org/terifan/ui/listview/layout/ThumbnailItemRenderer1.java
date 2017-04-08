273d3655eca2d15013d02d7d2103c345cacd347e
terifan <pinjata69@gmail.com>
2017-04-08 22:36:59 +0200

    update

--- src/org/terifan/ui/listview/layout/ThumbnailItemRenderer1.java
+++ src/org/terifan/ui/listview/layout/ThumbnailItemRenderer1.java
@@ -14,6 +14,7 @@ import org.terifan.ui.listview.ListViewColumn;
 import org.terifan.ui.listview.ListViewItem;
 import org.terifan.ui.listview.ListViewItemRenderer;
 import org.terifan.ui.listview.ListViewLayout;
+import org.terifan.ui.listview.ListViewModel;
 import org.terifan.ui.listview.Styles;
 import org.terifan.ui.listview.util.Anchor;
 import org.terifan.ui.listview.util.ImageResizer;
@@ -150,6 +151,10 @@ public class ThumbnailItemRenderer1<T extends ListViewItem> extends ListViewItem
 		return cat;
 	}
 
+	protected static final Color SELECTION_INNER_BORDER_COLOR = new Color(0,0,0,200);
+	protected static final Color SELECTION_OUTER_BORDER_COLOR = new Color(255,255,255);
+	protected static final Color LABEL_BACKGROUND_COLOR = new Color(0, 0, 0, 200);
+	protected static final Color THUMB_BACKGROUND_COLOR = new Color(30,30,30);
 
 	protected void paintRegularItem(ListView<T> aListView, T aItem, int aOriginX, int aOriginY, int aWidth, int aHeight, Graphics2D aGraphics)
 	{
@@ -162,16 +167,9 @@ public class ThumbnailItemRenderer1<T extends ListViewItem> extends ListViewItem
 		int w = aWidth;
 		int h = aHeight;
 
-		int labelHeight = getLabelHeight(aItem);
-
-		if (labelHeight < 0)
-		{
-			labelHeight = Math.abs(labelHeight * (aGraphics.getFontMetrics().getHeight() + 2));
-		}
-
 		BufferedImage icon = aItem.getIcon();
 
-		aGraphics.setColor(new Color(30, 30, 30));
+		aGraphics.setColor(THUMB_BACKGROUND_COLOR);
 		aGraphics.fillRect(aOriginX, aOriginY, aWidth, aHeight);
 
 		if (icon != null)
@@ -193,9 +191,6 @@ public class ThumbnailItemRenderer1<T extends ListViewItem> extends ListViewItem
 
 			int iw = icon.getWidth();
 			int ih = icon.getHeight();
-//			aGraphics.drawImage(icon, x + (w - iw) / 2, y + h - ih, iw, ih, null);
-
-			int distance = opacity / 20;
 
 			int sss = Math.max(iw, ih);
 			double sx = aWidth / (double)sss;
@@ -203,139 +198,77 @@ public class ThumbnailItemRenderer1<T extends ListViewItem> extends ListViewItem
 
 			int x0 = x + (w - (int)(sx * iw)) / 2;
 			int y0 = y + h - (int)(sy * ih);
-			aGraphics.drawImage(icon, x0, y0, x0 + (int)(sx * iw), y0 + (int)(sy * ih), distance, distance, iw - distance, ih - distance, null);
 
-			if (opacity > 0)
+			int distance = opacity / 20;
+
+			if (distance > 0)
 			{
-				aGraphics.setColor(new Color(30, 30, 30, opacity));
-				aGraphics.fillRect(x0, y0, (int)(sx * iw), (int)(sy * ih));
+				aGraphics.drawImage(icon, x0, y0, x0 + (int)(sx * iw), y0 + (int)(sy * ih), distance, distance, iw - distance, ih - distance, null);
 
-				requestRepaint(aListView);
+				if (opacity > 0)
+				{
+					aGraphics.setColor(new Color(30, 30, 30, opacity));
+					aGraphics.fillRect(x0, y0, (int)(sx * iw), (int)(sy * ih));
+
+					requestRepaint(aListView);
+				}
+			}
+			else
+			{
+				aGraphics.drawImage(icon, x0, y0, (int)(sx * iw), (int)(sy * ih), null);
 			}
 		}
 
-		String label = getLabel(aListView, aItem);
+		int labelHeight = getLabelHeight(aItem);
 
-		if (label != null && labelHeight > 0)
+		if (labelHeight != 0)
 		{
-			aGraphics.setColor(new Color(0, 0, 0, 200));
-			aGraphics.fillRect(x, y + h - labelHeight, w, labelHeight);
+			if (labelHeight < 0)
+			{
+				labelHeight = Math.abs(labelHeight * (aGraphics.getFontMetrics().getHeight() + 2));
+			}
 
-			TextRenderer.drawString(aGraphics, label, x + 2, y + h - labelHeight - 2, w - 4, labelHeight, Anchor.CENTER, aListView.getForeground(), null, false);
+			String label = getLabel(aListView, aItem);
+
+			if (label != null)
+			{
+				aGraphics.setColor(LABEL_BACKGROUND_COLOR);
+				aGraphics.fillRect(x, y + h - labelHeight, w, labelHeight);
+
+				TextRenderer.drawString(aGraphics, label, x + 2, y + h - labelHeight - 2, w - 4, labelHeight, Anchor.CENTER, aListView.getForeground(), null, false);
+			}
 		}
 
 		if (selected)
 		{
-			aGraphics.setColor(new Color(145, 206, 17, 64));
-			aGraphics.fillRect(aOriginX, aOriginY, aWidth, aHeight);
-
-			aGraphics.setColor(new Color(145, 206, 17));
-			for (int i = 0; i < 6; i++)
+			aGraphics.setColor(SELECTION_OUTER_BORDER_COLOR);
+			for (int i = 0; i < 3; i++)
 			{
-				aGraphics.drawRect(aOriginX + i, aOriginY + i, aWidth - 2 * i, aHeight - 2 * i);
+				aGraphics.drawRect(aOriginX + i, aOriginY + i, aWidth - 1 - 2 * i, aHeight - 1 - 2 * i);
+			}
+			aGraphics.setColor(SELECTION_INNER_BORDER_COLOR);
+			for (int i = 3; i < 5; i++)
+			{
+				aGraphics.drawRect(aOriginX + i, aOriginY + i, aWidth - 1 - 2 * i, aHeight - 1 - 2 * i);
 			}
 		}
 	}
 
 
-//	protected void paintFolder(ListView<T> aListView, T aItem, int aOriginX, int aOriginY, int aWidth, int aHeight, Graphics2D aGraphics)
-//	{
-//		changeVisibleState(aListView, aItem, true);
-//
-//		boolean selected = aListView.isItemSelected(aItem);
-//
-//		int x = aOriginX;
-//		int y = aOriginY;
-//		int w = aWidth;
-//		int h = aHeight;
-//
-//		int labelHeight = getLabelHeight(aItem);
-//
-//		if (labelHeight < 0)
-//		{
-//			labelHeight = Math.abs(labelHeight * (aGraphics.getFontMetrics().getHeight() + 2));
-//		}
-//
-//		BufferedImage icon = aItem.getIcon();
-//
-//		aGraphics.setColor(new Color(30, 30, 30));
-//		aGraphics.fillRect(aOriginX, aOriginY, aWidth, aHeight);
-//
-//		if (icon != null)
-//		{
-//			int opacity = 0;
-//
-//			if (aItem instanceof ViewPortStateControl)
-//			{
-//				ViewPortStateControl item = (ViewPortStateControl)aItem;
-//				long state = item.getViewPortState();
-//
-//				if (state == 0)
-//				{
-//					item.setViewPortState(state = System.currentTimeMillis());
-//				}
-//
-//				opacity = (int)Math.max(0, 255 - 255 * (System.currentTimeMillis() - state) / 500);
-//			}
-//
-//			int iw = icon.getWidth();
-//			int ih = icon.getHeight();
-////			aGraphics.drawImage(icon, x + (w - iw) / 2, y + h - ih, iw, ih, null);
-//
-//			int distance = opacity / 20;
-//
-//			int sss = Math.max(iw, ih);
-//			double sx = aWidth / (double)sss;
-//			double sy = aHeight / (double)sss;
-//
-//			int x0 = x + (w - (int)(sx * iw)) / 2;
-//			int y0 = y + h - (int)(sy * ih);
-//			aGraphics.drawImage(icon, x0, y0, x0 + (int)(sx * iw), y0 + (int)(sy * ih), distance, distance, iw - distance, ih - distance, null);
-//
-//			if (opacity > 0)
-//			{
-//				aGraphics.setColor(new Color(30, 30, 30, opacity));
-//				aGraphics.fillRect(x0, y0, (int)(sx * iw), (int)(sy * ih));
-//
-//				requestRepaint(aListView);
-//			}
-//		}
-//
-//		String label = getLabel(aListView, aItem);
-//
-//		if (label != null && labelHeight > 0)
-//		{
-//			aGraphics.setColor(new Color(0,0,0,128));
-//			aGraphics.fillRect(x, y+h-labelHeight, w, labelHeight);
-//			
-//			TextRenderer.drawString(aGraphics, label, x + 2, y + h - labelHeight - 2, w - 4, labelHeight, Anchor.NORTH, aListView.getForeground(), null, false);
-//		}
-//
-//		if (selected)
-//		{
-//			aGraphics.setColor(new Color(145, 206, 17, 64));
-//			aGraphics.fillRect(aOriginX, aOriginY, aWidth, aHeight);
-//
-//			aGraphics.setColor(new Color(145, 206, 17));
-//			for (int i = 0; i < 6; i++)
-//			{
-//				aGraphics.drawRect(aOriginX + i, aOriginY + i, aWidth - 2 * i, aHeight - 2 * i);
-//			}
-//		}
-//	}
 	protected String getLabel(ListView<T> aListView, T aItem)
 	{
-		String label = null;
-		for (int i = 0; i < aListView.getModel().getColumnCount(); i++)
+		ListViewModel<T> model = aListView.getModel();
+
+		for (int i = 0, n = model.getColumnCount(); i < n; i++)
 		{
-			ListViewColumn column = aListView.getModel().getColumn(i);
+			ListViewColumn column = model.getColumn(i);
 			if (column.isTitle())
 			{
-				label = column.getFormatter() == null ? Objects.toString(aItem.getValue(column)) : column.getFormatter().format(aItem.getValue(column));
-				break;
+				return column.getFormatter() == null ? Objects.toString(aItem.getValue(column)) : column.getFormatter().format(aItem.getValue(column));
 			}
 		}
-		return label;
+
+		return null;
 	}
 
 
